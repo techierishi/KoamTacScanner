9f285f0fb6acc0ebbc6393b27a3d7e15489f5a7e
Daniel Walker <daniel.walker@datasplice.com>
2013-11-19 11:14:51 -0700

    cordova wraps plugin in module. See: - http://cordova.apache.org/docs/en/3.1.0/plugin_ref_spec.md.html#Plugin%20Specification

--- www/koamtac.js
+++ www/koamtac.js
@@ -1,48 +1,45 @@
-cordova.define("cordova/plugin/KoamTacScanner",
 
-  function (require, exports, module) {
+var exec = require("cordova/exec");
 
-    var exec = require("cordova/exec");
+var KoamTacScanner = function () {
+};
 
-    var KoamTacScanner = function () {
-    };
+/**
+ * Triggers the scanner to scan.
+ *
+ * @param onScan Callback function. Called with the scan result when a scan event occurs.
+ * @param onError Called if the scanner cannot scan (possible due to enable() not having been called).
+ */
+KoamTacScanner.prototype.trigger = function (onScan, onError) {
+  exec(onScan, onError, 'KoamTacScanner', 'trigger', []);
+};
 
-    /**
-     * Triggers the scanner to scan.
-     *
-     * @param onScan Callback function. Called with the scan result when a scan event occurs.
-     * @param onError Called if the scanner cannot scan (possible due to enable() not having been called).
-     */
-    KoamTacScanner.prototype.trigger = function (onScan, onError) {
-      exec(onScan, onError, 'KoamTacScanner', 'trigger', []);
-    };
+/**
+ * Enable sets up a bluetooth socket connection to the paired device.
+ * Calls to trigger will fail unless the scanner has been 'enabled' first.
+ * This will also tell the plugin to start listening for scan events from the scanner.
+ *
+ * @param onScan Callback function. Called with the scan result when a scan event occurs.
+ * @param onError Called if there is a problem enabling the scanner plugin.
+ * @param options Options to configure the plugin (optional - currently unused).
+ */
+KoamTacScanner.prototype.enable = function (onScan, onError, options) {
+  options = options || {};
+  exec(onScan, onError, 'KoamTacScanner', 'enable', [options]);
+};
 
-    /**
-     * Enable sets up a bluetooth socket connection to the paired device.
-     * Calls to trigger will fail unless the scanner has been 'enabled' first.
-     * This will also tell the plugin to start listening for scan events from the scanner.
-     *
-     * @param onScan Callback function. Called with the scan result when a scan event occurs.
-     * @param onError Called if there is a problem enabling the scanner plugin.
-     * @param options Options to configure the plugin (optional - currently unused).
-     */
-    KoamTacScanner.prototype.enable = function (onScan, onError, options) {
-      options = options || {};
-      exec(onScan, onError, 'KoamTacScanner', 'enable', [options]);
-    };
+/**
+ * Disables the plugin. No scanning will be possible until a subsequent
+ * call to disable is made.
+ *
+ * @param onSuccess Called if the plugin is successfully disabled.
+ * @param onError Called if there is a problem disabling the scanner plugin.
+ */
+KoamTacScanner.prototype.disable = function (onSuccess, onError) {
+  exec(onSuccess, onError, 'KoamTacScanner', 'disable', []);
+};
 
-    /**
-     * Disables the plugin. No scanning will be possible until a subsequent
-     * call to disable is made.
-     *
-     * @param onSuccess Called if the plugin is successfully disabled.
-     * @param onError Called if there is a problem disabling the scanner plugin.
-     */
-    KoamTacScanner.prototype.disable = function (onSuccess, onError) {
-      exec(onSuccess, onError, 'KoamTacScanner', 'disable', []);
-    };
+// exports
+var plugin = new KoamTacScanner();
+module.exports = plugin;
 
-    // exports
-    var plugin = new KoamTacScanner();
-    module.exports = plugin;
-  });
