a06d5cadfa65340b5695c13175e93e8c70c00751
WebeWizard <webewizard@gmail.com>
2016-01-19 16:01:11 -0700

    Update js code
     to what is currently on the client master branch

--- www/koamtac.js
+++ www/koamtac.js
@@ -5,27 +5,30 @@ var KoamTacScanner = function () {
 };
 
 /**
- * Triggers the scanner to scan.
- *
- * @param onScan Callback function. Called with the scan result when a scan event occurs.
- * @param onError Called if the scanner cannot scan (possible due to enable() not having been called).
- */
-KoamTacScanner.prototype.trigger = function (onScan, onError) {
-  exec(onScan, onError, 'KoamTacScanner', 'trigger', []);
-};
-
-/**
  * Enable sets up a bluetooth socket connection to the paired device.
  * Calls to trigger will fail unless the scanner has been 'enabled' first.
  * This will also tell the plugin to start listening for scan events from the scanner.
  *
- * @param onScan Callback function. Called with the scan result when a scan event occurs.
+ * @param onSuccess Callback function. Called with scanner is initialized
  * @param onError Called if there is a problem enabling the scanner plugin.
- * @param options Options to configure the plugin (optional - currently unused).
+ * @param onScan Callback function. Called with the scan result when a scan event occurs.
  */
-KoamTacScanner.prototype.enable = function (onScan, onError, options) {
-  options = options || {};
-  exec(onScan, onError, 'KoamTacScanner', 'enable', [options]);
+KoamTacScanner.prototype.enable = function (onSuccess, onError, onScan) {
+  // deprecated - older versions only passed the scan and error callbacks and
+  // could not detect if the scanner didn't initialize properly
+  if(!onScan) {
+    onScan = onSuccess;
+  }
+  var handleCallback = function(results) {
+    results = JSON.parse(results);
+    if(results.status === 'success') {
+      onSuccess();
+    }
+    else if(results.scan) {
+      onScan(results.scan);
+    }
+  }
+  exec(handleCallback, onError, 'KoamTacScanner', 'enable', []);
 };
 
 /**
@@ -39,7 +42,16 @@ KoamTacScanner.prototype.disable = function (onSuccess, onError) {
   exec(onSuccess, onError, 'KoamTacScanner', 'disable', []);
 };
 
+/**
+ * Triggers the scanner to scan.
+ *
+ * @param onScan Callback function. Called with the scan result when a scan event occurs.
+ * @param onError Called if the scanner cannot scan (possible due to enable() not having been called).
+ */
+KoamTacScanner.prototype.trigger = function (onScan, onError) {
+  exec(onScan, onError, 'KoamTacScanner', 'trigger', []);
+};
+
 // exports
 var plugin = new KoamTacScanner();
 module.exports = plugin;
-
